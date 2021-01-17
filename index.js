2643335c21b142669debcedc338816b0c9272e4f
owenmoogk <owenmoogk@gmail.com>
2021-01-17 16:55:54 -0500

    cleaned up a lot
    there are duplicates, but its not visible and my code to get rid of them slowed the algorithm down a lot. i might loop back to it in the future

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 lineNumber = 0
-function drawLine(point1, point2){
+function drawLine(point1, point2){ // 8,5 and 8,0
     horizonal = point1[1] == point2[1]
     equation = ""
     if (horizonal){
         equation += "y = "+point1[1]
     }
     else{
-        equation += "x = "+point1[1]
+        equation += "x = "+point1[0]
     }
-    calculator.setExpression({id:lineNumber, latex:equation, polarDomain:{min:1, max:3}})
+    calculator.setExpression({id:lineNumber, latex:equation})
     lineNumber += 1
-
 }
 
-function drawRectangle(rectangleCoords){ // FIX ME
+function drawSquare(p1,p2){
+    console.log("i am called",p1,p2)
     p3 = [p1[0],p2[1]]
     p4 = [p2[0],p1[1]]
-    points = [p1,p2,p3,p4]
+    points = [p1,p3,p2,p4]
     for (i = 0; i < points.length-1; i++){
+        console.log(points[i], points[i+1])
         drawLine(points[i],points[i+1])
     }
     drawLine(points[0], points[points.length-1])
@@ -32,57 +33,27 @@ function drawPoints(points){
 }
 
 function findRectangles(points){
-    numberOfRectangles = 0
-    rectangles = []
+    points.sort()
+    numberOfSquares = 0
     for (p1 in points){
         for (p2 in points){
-
-            // i am checking for 2 diagonal points, and then finding the other 2 verticies.
-            // the 2 points must be not parallel and not equal
             notParallel = points[p1][0] != points[p2][0] && points[p1][1] != points[p2][1]
-            notEqual = p1 != p2
-
-            // point 3 and 4 are the other 2 points that must exist
-            point3 = [points[p2][0],points[p1][1]]
-            point4 = [points[p1][0],points[p2][1]]
-            point3Exists = false
-            point4Exists = false
-            for (i = 0; i < points.length; i++){
-                if (points[i][0] == point3[0] && points[i][1] == point3[1]){
-                    point3Exists = true
-                }
-                if (points[i][0] == point4[0] && points[i][1] == point4[1]){
-                    point4Exists = true
-                }
-            }
-            pointsExist = point4Exists && point3Exists
-            validRectangle = notEqual && notParallel && pointsExist
-            console.log(validRectangle)
-            rectangleCoords = []
-            if (validRectangle){
-                rectangleCoords = [points[p1], points[p2], [points[p1][0],points[p2][1]], [points[p2][0],points[p1][1]]]
-                rectangleCoords.sort()
-                if (rectangles.includes(rectangleCoords)){
-                    //pass
-                }
-                else{
-                    rectangles.push(rectangleCoords)
-                    drawRectangle(rectangleCoords)
-                    numberOfRectangles += 1
-                }
+            pointsExist = points.includes([points[p2][0],points[p1][1]]) && points.includes([points[p1][0],points[p2][1]])
+            if (points[p1] != points[p2] && notParallel){
+                numberOfSquares += 1
+                drawSquare(points[p1],points[p2])
             } 
         }
     }
-    console.log(numberOfRectangles)
-    return(numberOfRectangles)
 }
 
 // all coords
+// var coords = [
+//     [0,0],[5,0],[0,5],[5,5],[8,5],[8,0],[40,5],[5,54],[6,7],[40,0],[8,54]
+// ]
 var coords = [
-    [0,0],[5,0],[0,5],[5,5],[8,5],[8,0],[40,5],[5,54],[6,7],[40,0],[8,54]
+    [0,0],[5,0],[0,5],[5,5],[8,5],[8,0]
 ]
-var tocheck = []
-console.log(coords.includes(1))
 
 // desmos init
 var elt = document.getElementById('calculator');
