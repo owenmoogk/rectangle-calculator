9a150154939b3a9ed54bc14beb23f25a8f3b46a6
owenmoogk <owenmoogk@gmail.com>
2021-01-17 22:57:29 -0500

    added helper functions

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
 lineNumber = 0
-function drawLine(point1, point2){ // 8,5 and 8,0
-    horizonal = point1[1] == point2[1]
+// draw a line given 2 coords
+function drawLine(point1, point2){
     equation = ""
-    if (horizonal){
-        equation += "y = "+point1[1]
+    if (point1[0] == point2[0]){
+        equation += "x = "+point1[0]
     }
     else{
-        equation += "x = "+point1[0]
+        equation += "y = "+point1[1]
     }
     calculator.setExpression({id:lineNumber, latex:equation})
     lineNumber += 1
 }
 
-function drawSquare(p1,p2){
-    console.log("i am called",p1,p2)
-    p3 = [p1[0],p2[1]]
-    p4 = [p2[0],p1[1]]
-    points = [p1,p3,p2,p4]
-    for (i = 0; i < points.length-1; i++){
-        console.log(points[i], points[i+1])
-        drawLine(points[i],points[i+1])
+// returning the coords in an order that the draw square can understand
+function getRectangleCoordsInOrder(rectangleCoords){
+    orderedCoords = [[],[],[],[]]
+    indexesAppended = []
+    // first find 2 coords with the same x
+    for (i in rectangleCoords){
+        if (i == 0){
+            continue
+        }
+        if (rectangleCoords[0][0] == rectangleCoords[i][0]){
+            orderedCoords[0] = rectangleCoords[0]
+            orderedCoords[3] = rectangleCoords[i]
+            indexesAppended.push(i)
+            indexesAppended.push(0)
+            break
+        }
     }
-    drawLine(points[0], points[points.length-1])
+    // putting in the other 2
+    for (i in rectangleCoords){
+        if (i == 0 || i == indexesAppended[0]){
+            continue
+        }
+        if (rectangleCoords[i][1] == rectangleCoords[0][1]){
+            orderedCoords[1] = rectangleCoords[i]
+            indexesAppended.push(i)
+        }
+    }
+    for (i in indexesAppended){
+        indexesAppended[i] = parseInt(indexesAppended[i])
+    }
+    for (i in rectangleCoords){
+        if (indexesAppended.includes(parseInt(i))){
+            //pass
+        }
+        else{
+            console.log(i)
+            orderedCoords[2] = rectangleCoords[i]
+            break
+        }
+    }
+    return(orderedCoords)
 }
 
+// given sorted rectangle coords, draw the rectangle
+function drawRectangle(rectangleCoords){
+    rectangleCoords = getRectangleCoordsInOrder(rectangleCoords)
+    for (i = 0; i < rectangleCoords.length-1; i++){
+        console.log(rectangleCoords)
+        drawLine(rectangleCoords[i],rectangleCoords[i+1])
+    }
+    drawLine(rectangleCoords[0], rectangleCoords[rectangleCoords.length-1])
+}
+
+// given a set of points display them all on the graph
 function drawPoints(points){
     for (i = 0; i < points.length; i++){
         expId = "point"+i
